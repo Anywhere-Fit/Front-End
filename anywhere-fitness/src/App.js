ba5bedd8859d4d1e49045f8ecf92e3daed44a948
schandrupatla <55516943+schandrupatla@users.noreply.github.com>
2021-06-21 10:28:28 -0700

    Created new project anywhere-fitness

---
+++ anywhere-fitness/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Welcome to Anywhere Fintness!!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
