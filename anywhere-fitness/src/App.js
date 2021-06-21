615c8c2465eb196d1d574f5dc880e829dc409f40
ChristoherHenao <cq.henao@gmail.com>
2021-06-21 16:12:55 -0400

    login form structure done

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -3,13 +3,31 @@ import LoginPage from './components/LoginPage';
 import SignUpPage from './components/SignUpPage';
 import React, { useState } from 'react';
 
+// Created initial form values for Login Page
+const initialLoginFormValues = {
+  username: '',
+  password: '',
+}
+
+// Created initial form values for SignUp Page
+const initialSignUpFormValues = {
+  firstName: '',
+  lastName: '',
+  username: '',
+  password: '',
+}
+
+
 function App() {
-  
- 
+
+// Created state for SignUp form values
+ const [ signUpFormValues, setSignUpFormValues ] = useState(initialSignUpFormValues)
+
+//  Created state for Login form values
+ const [ loginFormValues, setLoginFormValues ] = useState(initialLoginFormValues)
   
   return (
     <div className="App">
-      
       <h1>Welcome to Anywhere Fitness!!</h1>
       <SignUpPage/>
     </div>
--- anywhere-fitness/src/components/SignUpPage.js
+++ anywhere-fitness/src/components/SignUpPage.js
@@ -4,7 +4,7 @@ import React from 'react'
 const SignUpPage = () => {
 
     return (
-        <div id=''>
+        <div id='signUpPage'>
             SignUp Page
 
             <form>
