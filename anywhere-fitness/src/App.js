4cd7f4d37f51a6bc02570425047b038930e2f604
John Laubscher <jlmemphiscommanders@gmail.com>
2021-06-23 15:25:03 -0500

    debugged and cleaned of extraneous code

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -1,43 +1,14 @@
-import './App.css';
-import React, { useEffect, useState } from "react";
-import ClassEditForm from './components/ClassEditForm'
-import ClassAddForm from './components/ClassAddForm'
-import axios from 'axios';
-import LoginPage from './components/LoginPage';
-import SignUpPage from './components/SignUpPage';
-
-// Created initial form values for Login Page
-const initialLoginFormValues = {
-  username: '',
-  password: '',
-}
-// Created initial form values for SignUp Page
-const initialSignUpFormValues = {
-  firstName: '',
-  lastName: '',
-  username: '',
-  password: '',
-}
+import "./App.css";
+import React from "react";
+import ClassEditForm from "./components/ClassEditForm";
+import ClassAddForm from "./components/ClassAddForm";
+import LoginPage from "./components/LoginPage";
+import SignUpPage from "./components/SignUpPage";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   //Private Route is available for all pages within the app.
   return (
-<<
-
-    </div>
-    <div className="App">
-
-      <header className="App-header">
-       <h1>Welcome to Anywhere Fintness!!</h1> 
-      </header>
-
-      <h1>Welcome to Anywhere Fitness!!</h1>
-      <SignUpPage/>
-
-
-        <div className="App">
-      <h1>Welcome to Anywhere Fitness!!</h1>
- 
     <Router>
       <div className="App">
         <h1>Welcome to Anywhere Fitness!!</h1>
--- anywhere-fitness/src/components/SignUpPage.js
+++ anywhere-fitness/src/components/SignUpPage.js
@@ -1,11 +1,7 @@
-
-import React, { useState }  from 'react'
-
-import { useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { useHistory } from "react-router";
-import Styled from 'styled-components'
-
+import Styled from "styled-components";
 
 // Styling for the SignUp page
 const StyledSignUpPage = Styled.div`
@@ -22,15 +18,8 @@ const StyledSignUpPage = Styled.div`
     }
     
 
-`
-
+`;
 // Created initial form values for SignUp Page
-const initialSignUpFormValues = {
-    firstName: '',
-    lastName: '',
-    username: '',
-    password: '',
-  }
 
 const SignUpPage = () => {
   const [signUpFormValues, setSignUpFormValues] = useState({
@@ -59,8 +48,9 @@ const SignUpPage = () => {
   };
 
   return (
-    <div id="signUpPage">
-      SignUp Page
+    <StyledSignUpPage id="signUpPage">
+      <h2>Register for Anywhere Fitness</h2>
+
       <form onSubmit={signup}>
         <label>First Name: </label>
         <input type="text" name="firstName" onChange={handleChange} value={signUpFormValues.firstName} />
@@ -78,92 +68,10 @@ const SignUpPage = () => {
         <input type="password" name="password" onChange={handleChange} value={signUpFormValues.password} />
         <br />
 
-        <button>Create Account</button>
+        <button>Register</button>
       </form>
-    </div>
+    </StyledSignUpPage>
   );
 };
 
 export default SignUpPage;
-
-
-    // Created state for SignUp form values
-    const [ signUpFormValues, setSignUpFormValues ] = useState(initialSignUpFormValues)
-
-    // Created a temporary state to hold the signed up members 
-    const [ members, setMembers ] = useState([])
-
-    // Created an update form values function for the SignUp page
-    const updateSignUpForm = (inputName, inputValue) => {
-        setSignUpFormValues({ ...signUpFormValues, [inputName]: inputValue })
-      }
-
-    // Created an onChange handler for the SignUp page
-      const onChangeSignUpForm = event => {
-        const { name, value } = event.target
-        updateSignUpForm(name, value)
-    }
-
-    // Created a submit form function for the SignUp form
-    const submitSignUpForm = () => {
-        const newMember = {
-            firstName: signUpFormValues.firstName,
-            lastName: signUpFormValues.lastName,
-            username: signUpFormValues.username,
-            password: signUpFormValues.password,
-        }
-
-        setMembers([newMember, ...members])
-        setSignUpFormValues(initialSignUpFormValues)
-    }
-
-    const onSubmitSignUp = (e) => {
-        e.preventDefault()
-        submitSignUpForm()
-    }
-
-    return (
-        <StyledSignUpPage id='signUpPage'>
-            <h2>Register for Anywhere Fitness</h2>
-
-            <form onSubmit={onSubmitSignUp}>
-                <label>First Name: </label>
-                    <input
-                    type='text'
-                    name='firstName'
-                    onChange={onChangeSignUpForm}
-                    value={signUpFormValues.firstName}
-                    /><br/>
-
-                <label>Last Name: </label>
-                    <input
-                    type='text'
-                    name='lastName'
-                    onChange={onChangeSignUpForm}
-                    value={signUpFormValues.lastName}
-                    /><br/>
-
-                <label>Username: </label>
-                    <input
-                    type='text'
-                    name='username'
-                    onChange={onChangeSignUpForm}
-                    value={signUpFormValues.username}
-                    /><br/>
-
-                <label>Password: </label>
-                    <input
-                    type='password'
-                    name='password'
-                    onChange={onChangeSignUpForm}
-                    value={signUpFormValues.password}
-                    /><br/>
-
-                    <button>Register</button>
-            </form>
-        </StyledSignUpPage>
-    )
-}
-
-export default SignUpPage
-
