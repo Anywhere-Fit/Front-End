9e1dcfb08d5045cf6eeae77faa54403bc5d9a5a4
ChristoherHenao <cq.henao@gmail.com>
2021-06-23 12:30:19 -0400

    basic form styling done

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -10,9 +10,7 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome to Anywhere Fitness!!</h1>
-      <LoginPage/>
-      <SignUpPage/>
-      <ClassListPage/>
+ 
     </div>
   );
 }
