74efb0f67d97ebbf06a8744c6c5f3475458cca64
schandrupatla <55516943+schandrupatla@users.noreply.github.com>
2021-06-23 09:54:10 -0700

    Added Actions, reducers, Add and Edit forms

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -1,11 +1,16 @@
 
 import './App.css';
+import React, { useEffect, useState } from "react";
+import ClassEditForm from './components/ClassEditForm'
+import ClassAddForm from './components/ClassAddForm'
+import axios from 'axios';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Welcome to Anywhere Fintness!!</h1>
+       <h1>Welcome to Anywhere Fintness!!</h1> 
       </header>
     </div>
   );
--- anywhere-fitness/src/index.js
+++ anywhere-fitness/src/index.js
@@ -2,11 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {createStore, applyMiddleware} from 'redux';
+import { rootReducer } from './reducers';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+
+const store = createStore(rootReducer,applyMiddleware(logger,thunk))
 
 ReactDOM.render(
+ 
   <React.StrictMode>
+     {/*  react-redux */}
+      {/* <Provider store={store}> */}
     <App />
+     {/* </Provider> */}
+     {/* </Router> */}
   </React.StrictMode>,
+ 
   document.getElementById('root')
 );
 
